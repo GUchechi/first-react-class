f445eca50066d38f6f59dc02e7c3a8048f156f60
Njoku Godswill Uchendu Uchechi <godswilluchendu7@gmail.com>
2022-06-28 07:06:19 -0700

    My  First Class on React

---
+++ src/Females.js
@@ -0,0 +1,13 @@
+const Females = () => {
+    return (
+        <div>
+            <ul>
+                <li>Chidinma</li>
+                <li>Justina</li>
+                <li>Rachel</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Females;
---
+++ src/Males.js
@@ -0,0 +1,13 @@
+const Males = () => {
+    return (
+        <div>
+            <ul>
+                <li>Godswill</li>
+                <li>Emmanuel</li>
+                <li>Caeser</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Males;
